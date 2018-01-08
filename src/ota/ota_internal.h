f3d18921e5f56a22ee0e9ae14a503d9bfc62df26
zhj2232006 <zhj2232006@gmail.com>
2018-01-08 16:00:44 +0800

    add default OTA_SIGNAL_CHANNEL=1 (MQTT)

--- src/ota/ota_internal.h
+++ src/ota/ota_internal.h
@@ -21,6 +21,10 @@
 #ifndef _OTA_INTERNAL_H_
 #define _OTA_INTERNAL_H_
 
+#ifndef OTA_SIGNAL_CHANNEL
+#define OTA_SIGNAL_CHANNEL 1
+#endif
+
 typedef void (*ota_cb_fpt)(void *pcontext, const char *msg, uint32_t msg_len);
 
 #endif /* _OTA_INTERNAL_H_ */
