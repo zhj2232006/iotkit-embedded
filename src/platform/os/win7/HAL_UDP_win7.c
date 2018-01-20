34862dcde2f71d00ce91e51c997012d699b89321
jack <zhj2232006@gmail.com>
2018-01-20 23:52:18 +0800

    TOOD Windows UDP api

--- src/platform/os/win7/HAL_UDP_win7.c
+++ src/platform/os/win7/HAL_UDP_win7.c
@@ -17,46 +17,68 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <ws2tcpip.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <Winbase.h>
 #include <string.h>
-#include <errno.h>
-// #include <sys/select.h>
-// #include <sys/socket.h>
-// #include <netinet/in.h>
-// #include <arpa/inet.h>
-// #include <netdb.h>
-// #include <unistd.h>
-// #include <pthread.h>
 
 #include "iot_import.h"
 
+#define PLATFORM_WINSOCK_LOG(format, ...)                                                      \
+    do                                                                                         \
+    {                                                                                          \
+        HAL_Printf("LINUXSOCK %u %s() | " format "\n", __LINE__, __FUNCTION__, ##__VA_ARGS__); \
+        fflush(stdout);                                                                        \
+    } while (0);
+
+#define PLATFORM_WINSOCK_PERROR(log)                                                                                \
+    do                                                                                                              \
+    {                                                                                                               \
+        char* s = NULL;                                                                                             \
+        FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, \
+                       NULL,                                                                                                   \
+                       WSAGetLastError(),                                                                                      \
+                       MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),                                                           \
+                       (LPSTR)&s,                                                                                              \
+                       0,                                                                                                      \
+                       NULL);                                                                                                  \
+        HAL_Printf("[SOCK]%d %s() %s: %s", __LINE__, __FUNCTION__, log, s);                                         \
+        LocalFree(s);                                                                                               \
+    } while (0);
+#include "iot_import_coap.h"
+
 void* HAL_UDP_create(char* host, unsigned short port)
 {
-	return NULL;
+    PLATFORM_WINSOCK_PERROR("todo implement");
 }
 
 void HAL_UDP_close(void* p_socket)
 {
+    PLATFORM_WINSOCK_PERROR("todo implement");
 }
 
 int HAL_UDP_write(void* p_socket,
                   const unsigned char* p_data,
                   unsigned int datalen)
 {
-	return 0;
+    PLATFORM_WINSOCK_PERROR("todo implement");
 }
 
 int HAL_UDP_read(void* p_socket,
                  unsigned char* p_data,
                  unsigned int datalen)
 {
-	return 0;
+    PLATFORM_WINSOCK_PERROR("todo implement");
 }
 
-int HAL_UDP_readTimeout(void* p_socket,
-                        unsigned char* p_data,
-                        unsigned int datalen,
-                        unsigned int timeout)
+int HAL_UDP_readTimeout(void* p_socket, unsigned char* p_data,
+                        unsigned int datalen, unsigned int timeout)
 {
-	return 0;
+    PLATFORM_WINSOCK_PERROR("todo implement");
+}
+
+int HAL_UDP_resolveAddress(const char* p_host, char addr[128])
+{
+    PLATFORM_WINSOCK_PERROR("todo implement");
 }
